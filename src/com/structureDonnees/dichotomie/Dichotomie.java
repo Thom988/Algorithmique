4c0d935da6e44186805e281aa8b903f202acefc8
Thom988 <t.quillet@hotmail.fr>
2023-12-26 22:32:20 +0100

    Exercice recherche dichotomique

---
+++ src/com/structureDonnees/dichotomie/Dichotomie.java
@@ -0,0 +1,54 @@
+package com.structureDonnees.dichotomie;
+
+public class Dichotomie {
+	
+	public static void afficherTableau(int[] tableau) {
+		System.out.println("afficher tableau : ");
+		for (int i = 0; i < tableau.length; i++) {
+			System.out.println(tableau[i]);
+		}
+	}
+	
+	public static String rechercheDicho(int[] tableau, int target) {
+		boolean trouv = false;
+		int moy = 0; 
+		int min = tableau[0];
+		int max = tableau[tableau.length-1];
+		System.out.println("recherche dichotomique de " + target);
+		while (trouv == false && min <= max ) {
+			moy = (min + max)/2;
+			System.out.println("min : " + min + " max : " + max + " moy : " + moy);
+			if (tableau[moy] == target ) {
+				trouv = true;
+			} else if (tableau[moy] < target) {
+				min = moy + 1;
+			} else {
+				max = moy - 1;
+			}
+		}
+		String message = "" + target;
+		if (trouv == true) {
+			message += ", existe";
+		} else {
+			message += ", n'existe pas";
+			System.out.println("min : " + min + " max : " + max + " moy : " + moy);
+		}
+		return message;
+	}
+	
+	public static int[] remplirTableau(int[] tableau) {
+		for (int i=0; i<tableau.length; i++) {
+			tableau[i] = i;
+		}
+		return tableau;
+	}
+
+	public static void main(String[] args) {
+		int[] tableau = new int[20];
+		int target = 15;
+		tableau = remplirTableau(tableau);
+		afficherTableau(tableau);
+		System.out.println(rechercheDicho(tableau, target));
+	}
+
+}
